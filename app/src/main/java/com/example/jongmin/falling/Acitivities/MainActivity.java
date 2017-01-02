52fa003188790fe2d41db730b57e5823c40af6d5
Jongmin <jinjm37@gmail.com>
2017-01-02 15:28:20 +0900

    Main

---
+++ app/src/main/java/com/example/jongmin/falling/Acitivities/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.jongmin.falling.Acitivities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.jongmin.falling.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
